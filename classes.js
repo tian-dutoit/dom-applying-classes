900f00f6a46724b3170f6cd818b60021e2a09a84
Tian <Tian@Louises-iMac-2.local>
2017-12-02 21:55:05 +1300

    added visible formula

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -31,3 +32,7 @@ function three () {
   three.classList.add('red');
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var visible = document.getElementsByClassName('invisible')[0];
+  visible.classList.add('visible');
+}
